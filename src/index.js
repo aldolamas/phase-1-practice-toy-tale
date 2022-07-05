ba67662c5e18664cfcea9e6de75278aa3b7091ff
Aldo Lamas <aldolamas01@gmail.com>
2022-07-05 15:10:23 -0500

    commit tree 4816a91fe41a34a35ea4ee623203574dfe851b31

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 let addToy = false;
 
-document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -11,5 +10,82 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
       toyFormContainer.style.display = "none";
     }
+
+    toyFormContainer.addEventListener('submit', (event) => {
+      e.preventDefault()
+      postToy(event.target.name.value, event.target.image.value)
+    })
   });
-});
+
+function getToys() {
+  return fetch('http://localhost:3000/toys')
+    .then(response => response.json())
+  }
+
+function renderToy(toy) {
+  const toyCard = document.createElement('div')
+  toyCard.setAttribute('class', 'card')
+  const h2 = document.createElement('h2')
+  h2.innerText = toy.name
+  const img = document.createElement('img')
+  img.setAttribute('class', 'toy-avatar')
+  img.setAttribute('src', toy.image)
+  const p = document.createElement('p')
+  p.setAttribute('class', 'like')
+  p.innerText = toy.likes
+  const btn = document.createElement('button')
+  btn.setAttribute('class', 'like-btn')
+  btn.setAttribute('id', toy.id)
+  btn.innerText = 'Like ❤️'
+  btn.addEventListener('click', (e) => {
+    increaseLikes(e)
+  })
+  const toyBox = document.getElementById("toy-collection")
+  toyCard.append(h2, img, p, btn)
+  toyBox.append(toyCard)
+}
+
+function postToy(name, url) {
+  fetch('http://localhost:3000/toys', {
+    method: 'POST',
+    headers: {
+
+    "Content-Type": "application/json",
+    Accept: "application/json"
+  },
+
+    body: JSON.stringify({
+      "name": name,
+      "image": url,
+      "likes": 0
+    })
+  })
+  .then(response => response.json())
+  .then(data => renderToy(data))
+}
+
+function increaseLikes(event) {
+  event.preventDefault()
+  document.getElementsByClassName('like').innerText + 1
+  const newNumberOfLikes = parseInt(event.target.previousElementSibling.innerText) + 1
+  fetch(`http://localhost:3000/toys/${event.target.id}`, {
+    method: 'PATCH',
+    headers:
+{
+  "Content-Type": "application/json",
+  Accept: "application/json"
+},
+
+body: JSON.stringify({
+  "likes": newNumberOfLikes
+})
+  })
+.then(response => response.json())
+.then(event => event.target.previousElementSibling.innerText = `${newNumberOfLikes}`)
+}
+
+getToys().then(toys => {
+  toys.forEach(toy => {
+    renderToy(toy)
+  })
+})
